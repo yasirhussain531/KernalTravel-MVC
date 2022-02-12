d738cd7506419913f143b1a51461c3360723ad0a
unknown <yasirhussain531@gmail.com>
2022-02-12 13:10:56 +0500

    MVC Travel Website

---
+++ KernalTravel/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KernalTravel.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PopularPlaces()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public ActionResult Booking()
+        {
+            return View();
+        }
+    }
+}
